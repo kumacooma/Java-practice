533ff94e789977a0abc941911e9ddfe85db17586
bella <copperbook123@naver.com>
2024-02-07 09:34:57 +0900

    daily quiz

---
+++ src/dailyqz/LoginExample.java
@@ -0,0 +1,48 @@
+package dailyqz;
+
+public class LoginExample {
+
+    public static void main(String[] args) {
+        try {
+            login("white", "12345");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            login("blue", "54321");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void login(String id, String password)
+            throws NotExistsIdException, WrongPasswordException {
+        if (!id.equals("blue")) {
+            throw new NotExistsIdException("아이디가 존재하지 않습니다.");
+        }
+
+        if (!password.equals("12345")) {
+            throw new WrongPasswordException("패스워드가 틀립니다.");
+        }
+    }
+}
+class WrongPasswordException extends Exception {
+
+    public WrongPasswordException() {
+    }
+
+    public WrongPasswordException(String message) {
+        super(message);
+    }
+}
+
+class NotExistsIdException extends Exception {
+
+    public NotExistsIdException() {
+    }
+
+    public NotExistsIdException(String message) {
+        super(message);
+    }
+}
