fe763da9da1f881f805037bd1ec6b3c6f551d8e3
bella <copperbook123@naver.com>
2024-02-07 23:30:17 +0900

    코테 연습 추가

--- src/Programmers.java
+++ src/Programmers.java
@@ -64,4 +64,76 @@ public class Programmers {
         }
         return answer;
     }
+
+
+   /* 정수 리스트 num_list가 주어질 때, 마지막 원소가 그전 원소보다 크면 마지막 원소에서
+     그전 원소를 뺀 값을 마지막 원소가 그전 원소보다 크지 않다면
+     마지막 원소를 두 배한 값을 추가하여
+     return하도록 solution 함수를 완성해주세요.*/
+    public int[] lastIndex(int[] num_list) {
+        if (num_list.length < 2) {
+            throw new IllegalArgumentException("리스트의 길이는 최소 2 이상이어야 합니다.");
+        }
+
+        int[] result = new int[num_list.length + 1];
+        result[0] = num_list[0];
+
+        for (int i = 1; i < num_list.length; i++) {
+            result[i - 1] = num_list[i - 1];
+            result[i] = num_list[i];
+            if (num_list[i] > num_list[i - 1]) {
+                result[i + 1] = num_list[i] - num_list[i - 1];
+            } else {
+                result[i + 1] = num_list[i] * 2;
+            }
+        }
+
+        return result;
+    }
+
+    //정수 찾기 n과 같으면 1반환 다르면 0반환
+    public int findInterger(int[] num_list, int n) {
+
+        for (int i = 0; i < num_list.length; i++) {
+            if (num_list[i] == n) {
+                return 1;
+            }
+        }
+        return 0;
+    }
+    //start_num부터 end_num까지의 숫자를 차례로 담은 리스트를 return
+    public int[] countUp(int start_num, int end_num) {
+        int[] answer = new int[end_num-start_num+1];
+
+        answer[0] = start_num;
+
+        for (int i = 0; i < answer.length; i++) {
+
+            answer[answer.length-1] = end_num;
+            answer[i] = start_num++;
+        }
+        return answer;
+    }
+
+    //arr의 각 원소에 대해 값이 50보다 크거나 같은 짝수라면 2로 나누고, 50보다 작은 홀수라면 2를 곱합니다.
+    public int[] translateArray(int[] arr) {
+
+        int[] answer = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i]>=50&&arr[i]%2==0){
+                answer[i] = arr[i]/2;
+            }
+            else if(arr[i]<=50&&arr[i]%2!=0){
+                answer[i] = arr[i]*2;
+            }
+            else{
+                answer[i] = arr[i];
+            }
+        }
+
+        return answer;
+    }
+
+
+
 }
