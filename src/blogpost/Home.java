a590787a0309b0300521d3e0a3448d1a02f38f7f
bella <copperbook123@naver.com>
2024-02-07 09:33:14 +0900

    블로그 포스팅 할 주제 연습

---
+++ src/blogpost/Home.java
@@ -0,0 +1,16 @@
+package blogpost;
+
+public class Home {
+
+    public static void main(String[] args) {
+
+        Me me = new Me();
+        System.out.println(me);
+
+        Me me2 = new Me();
+        System.out.println(me2);
+        System.out.println(me2.name);
+
+
+    }
+}
---
+++ src/blogpost/Me.java
@@ -0,0 +1,19 @@
+package blogpost;
+
+public class Me {
+
+    String name;
+    String phonenumber;
+    String hobby;
+
+    public Me(){
+
+        System.out.println("나 입니다.");
+    }
+
+    public Me(String name){
+        this.name = name;
+        System.out.println(name);
+    }
+
+}
